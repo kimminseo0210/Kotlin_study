1d5863709c0f91572adcd09e8b200b2f5bc41353
kimminseo0210 <kmsk030210@gmail.com>
2026-03-19 22:33:55 +0900

    완료 : step7 진행중 : - [ 26.03.19 ]

--- src/step7(2차원배열)/Q2563.kt
+++ src/step7(2차원배열)/Q2563.kt
@@ -1,5 +1,24 @@
 package `step7(2차원배열)`
 
-fun main() {
+import java.util.StringTokenizer
 
+fun main() {
+    val br = System.`in`.bufferedReader()
+    val bw = System.out.bufferedWriter()
+    var sb = StringBuilder()
+
+    val testCase = br.readLine()?.toInt() ?: 0
+
+    var paperMatrix = Array(100) { IntArray(100) { 0 } }
+
+    repeat (testCase) {
+        var st = StringTokenizer(br.readLine() ?: "")
+        val x = st.nextToken().toInt()
+        val y = st.nextToken().toInt()
+        for (i in x until x+10) {
+            for (j in y until y+10) paperMatrix[i][j] = 1
+        }
+    }
+    var paperCount = paperMatrix.sumOf { row -> row.count { it == 1 } }
+    println(paperCount)
 }
